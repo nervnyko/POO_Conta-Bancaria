a406f6f4462f8984718582c8c9178533c36f9f24
Nycholas <83355390+nervnyko@users.noreply.github.com>
2025-09-19 20:10:52 -0300

    pequenos comentários e match de conta poupança

--- main.py
+++ main.py
@@ -50,11 +50,13 @@ print ("Digite o número da ação que deseja realizar e em seguida aperte Enter
 print ("1 - Conta Corrente \n2- Conta Poupança")
 conta_type = int (input ())
 
-while True:
+#loop para conta corrente.
+while conta_type == 1:
     print ("\nO que deseja fazer?")
     print ("\n1 - Exibir saldo \n2 - Depositar \n3 - Sacar \n4 - Mudar conta \n5 - Sair")
     action = int (input ())
-      
+    
+    #match de ação de acordo com o input
     match action:
         case 1: 
             conta1.ver_saldo()
@@ -80,9 +82,48 @@ while True:
         
         case 4:
             print ("Mudando de conta...")
+            conta_type = 2
                     
         case 5:
             break
 
-                            
-            
+#loop caso conta poupança
+while conta_type == 2:                            
+    print ("\nO que deseja fazer?")
+    print ("\n1 - Exibir saldo \n2 - Depositar \n3 - Sacar \n4 - Mudar conta \n5 - Ver rendimento \n6 - Sair")
+    action = int (input ())
+    
+    #match de ação de acordo com o input
+    match action:
+        case 1: 
+            conta2.ver_saldo()
+
+        case 2: 
+            try:
+                valor = float(input ("\nDigite o valor que deseja depositar: "))
+                conta2.depositar(valor)
+                conta = conta2
+                print(f"Depósito realizado com sucesso! Seu saldo final é de: R$ {conta.pegar_saldo():.2f}")
+
+            except ValueError as e:
+                print ("\nErro.{e}")
+                    
+        case 3:
+            try:
+                valor = float (input("\nDigite o valor que deeseja sacar: "))
+                conta2.sacar(valor)
+                print (f"\nSaque realizado com sucesso, seu saldo final é de: R$ {conta.pegar_saldo():.2f}")
+
+            except ValueError as e:
+                print ("\nErro. {e}")
+        
+        case 4:
+            print ("Mudando de conta...")
+            conta_type = 1
+
+        case 5: 
+            conta2.rendimento ()
+            print (f"Seu saldo com o rendimento fica: R$ {conta2._saldo}")
+
+        case 6:
+            break
